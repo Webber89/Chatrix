8de95e14edc93793468ffc17814bd837d5aa976f
pcfascinated <mikkelclement91@gmail.com>
2013-09-24 21:47:51 +0200

    sendMessage method added

--- Chatrix/src/client/MessageHandler.java
+++ Chatrix/src/client/MessageHandler.java
@@ -1,17 +1,42 @@
 package client;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 import server.Message;
 
-
 public class MessageHandler {
+
 	private static MessageHandler messageHandler = new MessageHandler();
-	
+	private static String host = "1.1.1.0";
+	private int port = 9999;
+
 	public static MessageHandler getInstance() {
 		return messageHandler;
 	}
-	
+
 	public void receiveMessage(Message message) {
 		// TODO afkodning af message
+
 	}
-	
+
+	public void sendMessage(Message message) {
+
+		Socket clientSocket;
+		try {
+			clientSocket = new Socket(host, port);
+			System.out.println("Socket with IP: "
+					+ clientSocket.getInetAddress().getHostAddress());
+			OutputStream outputStream = clientSocket.getOutputStream();
+			OutputStreamWriter outputWriter = new OutputStreamWriter(
+					outputStream);
+			BufferedWriter bWriter = new BufferedWriter(outputWriter);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
