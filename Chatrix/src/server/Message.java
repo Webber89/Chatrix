da07195ef963ab79d13515d08f825380b6ad0ce5
Emil Thorenfeldt <emildinho@gmail.com>
2013-09-25 11:16:23 +0200

    Increased JSON implementation

--- Chatrix/src/server/Message.java
+++ Chatrix/src/server/Message.java
@@ -1,22 +1,38 @@
 package server;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 public class Message {
-	private HashMap<String, String> keyValuePairs = new HashMap<String, String>();
+	private LinkedHashMap<String, String> keyValuePairs = new LinkedHashMap<String, String>();
 	
 	public void addKeyValue(String key, String value) {
 		keyValuePairs.put(key, value);
 	}
 	
 	public Message(String messageType) {
-		keyValuePairs.put("Type", messageType);
+		keyValuePairs.put("type", messageType);
 	}
 	
-	public String toJson() {
+//	public Message(String json) {
+//		
+//	}
+	
+	// To JSON and back again
+	public String toJson() throws JsonParseException, JsonMappingException, IOException {
 		Gson gson = new Gson();
-		return gson.toJson(keyValuePairs);
+		String json = gson.toJson(keyValuePairs);
+		System.out.println(json);
+		@SuppressWarnings("unchecked")
+		LinkedHashMap<String, Object> test = new ObjectMapper().readValue(json, LinkedHashMap.class);
+		for (String s : test.keySet())
+			System.out.println("key: " + s + ", value: " + test.get(s));
+		
+		return json;
 	}
 }
--- Chatrix/src/server/TestConnection.java
+++ Chatrix/src/server/TestConnection.java
@@ -7,24 +7,52 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
+import java.util.HashMap;
 
 import client.MessageHandler;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonStreamParser;
+import com.google.gson.stream.JsonReader;
 
 public class TestConnection {
 
 	public static void main(String[] args) throws IOException {
-		new TestConnection();
+		Message message = new Message("MSG");
+		message.addKeyValue("room", "public");
+		message.addKeyValue("content", "hello, world");
+		message.toJson();
+//		new TestConnection();
 	}
 
 	public TestConnection() throws IOException {
 		MessageHandler.getInstance().sendMessage("asdsad");
 //		System.out.println(getIP());
-//		ServerSocket serverSocket = new ServerSocket(7000);
-//		Socket socket = serverSocket.accept();
+		ServerSocket serverSocket = new ServerSocket(7000);
+		Socket socket = serverSocket.accept();
 //		System.out.println(socket.getInetAddress().getHostAddress());
 //		
-//		InputStream is = socket.getInputStream();
-//        InputStreamReader isr = new InputStreamReader(is);
+		InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        
+        JsonStreamParser parser = new JsonStreamParser(isr);
+        JsonElement element = parser.next();
+        JsonObject jObject = element.getAsJsonObject();
+        jObject.get("type");
+        
+        JsonReader reader = new JsonReader(isr);
+        reader.beginArray();
+        HashMap<String, String> keyValuePairs = new HashMap<String, String>();
+        while (reader.hasNext()) {
+        	reader.beginObject();
+        	String key = reader.nextName();
+        	String value = reader.nextString();
+        	keyValuePairs.put(key, value);
+        	reader.endObject();
+        }
+        
+        reader.close();
 //        BufferedReader br = new BufferedReader(isr);
 //        boolean done = false;
 //      
@@ -35,10 +63,10 @@ public class TestConnection {
 //            msgOut = msgOut.concat(message);  
 //        }
 //        System.out.println(msgOut);
-//		serverSocket.close();
+		serverSocket.close();
 	}
 
-	private String getIP() throws IOException {
+	protected String getIP() throws IOException {
 		URL amazonIP = new URL("http://checkip.amazonaws.com");
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 		                amazonIP.openStream()));
