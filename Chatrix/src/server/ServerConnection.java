0e3008dbbc55d6b11d8b3f96eb002db0d50a45ab
pcfascinated <mikkelclement91@gmail.com>
2013-10-02 14:24:44 +0200

    Stop server added / chat not working

--- Chatrix/src/server/ServerConnection.java
+++ Chatrix/src/server/ServerConnection.java
@@ -1,49 +1,55 @@
-
 package server;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerConnection implements Runnable {
-	private int port;
-	boolean running = true;
+public class ServerConnection implements Runnable
+{
+    private int port;
+    public boolean running = true;
+    public Socket socket;
+    public ServerSocket serverSocket;
 
-	public ServerConnection(int port) {
-		this.port = port;
+    public ServerConnection(int port)
+    {
+	this.port = port;
+    }
+
+    @Override
+    public void run()
+    {
+	openClientListener();
+    }
+
+    protected void openClientListener()
+    {
+	try
+	{
+	    serverSocket = new ServerSocket(port);
+	    while (running)
+	    {
+		System.out.println("Waiting for new clients");
+		socket = serverSocket.accept();
+		Client testClient = new Client(socket);
+		testClient.output.send("Test \n");
+	    }
+	} catch (IOException e)
+	{
+	    System.out.println("Interrupted");
 	}
+    }
 
-	@Override
-	public void run() {
-		openClientListener();
+    public void testMessage()
+    {
+	Message message = new Message(Message.Type.JOIN);
+	try
+	{
+	    System.out.println(message.toJson());
+	} catch (Exception e)
+	{
+	    e.printStackTrace();
 	}
-
-	protected void openClientListener() {
-		ServerSocket serverSocket;
-		try {
-			serverSocket = new ServerSocket(port);
-		while (running) {
-		    System.out.println("Waiting for new clients");
-			Socket socket = serverSocket.accept();
-			Client testClient = new Client(socket);
-			testClient.output.send("Test \n");
-		}
-		serverSocket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	
-	
-	public void testMessage() {
-		Message message = new Message(Message.Type.JOIN);
-		try {
-			System.out.println(message.toJson());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-
+    }
 }
--- Chatrix/src/server/ServerGUI.java
+++ Chatrix/src/server/ServerGUI.java
@@ -23,8 +23,12 @@ public class ServerGUI extends JFrame
     private JTextField textField;
     private final ButtonGroup buttonGroup_3 = new ButtonGroup();
     ServerController sc = new ServerController();
+    private JToggleButton tglbtnPrivate;
+    private JToggleButton tglbtnWan;
+    private JToggleButton rdbtnTcp;
+    private JToggleButton rdbtnUdp;
+    private JButton btnStop;
 
-    
     /**
      * Create the frame.
      */
@@ -37,59 +41,83 @@ public class ServerGUI extends JFrame
 	setTitle("Server");
 	setContentPane(contentPane);
 	contentPane.setLayout(null);
-	
+
 	JPanel panel = new JPanel();
 	panel.setBounds(10, 11, 263, 157);
 	panel.setBorder(BorderFactory.createLineBorder(Color.gray));
 	contentPane.add(panel);
 	panel.setLayout(null);
-	
+
 	JButton btnNewButton = new JButton("Start Server");
-	btnNewButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-		    sc.createServer("TCP", false, 7000);
+	btnNewButton.addActionListener(new ActionListener()
+	{
+	    public void actionPerformed(ActionEvent arg0)
+	    {
+		boolean isPrivate = false;
+		if (tglbtnPrivate.isSelected())
+		{
+		    isPrivate = true;
 		}
+		String conType = "TCP";
+		if (rdbtnUdp.isSelected())
+		{
+		    conType = "UDP";
+		}
+		sc.createServer(conType, isPrivate,
+			Integer.parseInt(textField.getText()));
+		btnStop.setEnabled(true);
+	    }
 	});
-	btnNewButton.setBounds(43, 126, 91, 23);
+	btnNewButton.setBounds(10, 126, 109, 23);
 	panel.add(btnNewButton);
-	
-	JToggleButton rdbtnTcp = new JToggleButton("TCP");
+
+	rdbtnTcp = new JToggleButton("TCP");
 	rdbtnTcp.setBounds(107, 11, 68, 23);
 	panel.add(rdbtnTcp);
 	buttonGroup_1.add(rdbtnTcp);
 	rdbtnTcp.setSelected(true);
-	
-	JToggleButton rdbtnUdp = new JToggleButton("UDP");
+
+	rdbtnUdp = new JToggleButton("UDP");
 	rdbtnUdp.setBounds(185, 11, 68, 23);
 	panel.add(rdbtnUdp);
 	buttonGroup_1.add(rdbtnUdp);
-	
+
 	textField = new JTextField();
 	textField.setBounds(107, 76, 68, 20);
 	panel.add(textField);
 	textField.setColumns(10);
-	
+
 	JLabel lblPort = new JLabel("Port");
 	lblPort.setBounds(10, 79, 46, 14);
 	panel.add(lblPort);
-	
+
 	JLabel lblConnectionType = new JLabel("Connection type");
 	lblConnectionType.setBounds(10, 15, 87, 14);
 	panel.add(lblConnectionType);
-	
+
 	JLabel lblWanlocal = new JLabel("WAN / LAN");
 	lblWanlocal.setBounds(10, 46, 61, 14);
 	panel.add(lblWanlocal);
-	
-	JToggleButton tglbtnWan = new JToggleButton("Wan");
+
+	tglbtnWan = new JToggleButton("Wan");
 	buttonGroup_3.add(tglbtnWan);
 	tglbtnWan.setBounds(107, 42, 68, 23);
 	panel.add(tglbtnWan);
-	
-	JToggleButton tglbtnPrivate = new JToggleButton("Local");
+
+	tglbtnPrivate = new JToggleButton("Local");
 	buttonGroup_3.add(tglbtnPrivate);
 	tglbtnPrivate.setBounds(185, 42, 68, 23);
 	panel.add(tglbtnPrivate);
+	
+	btnStop = new JButton("Stop Server");
+	btnStop.setEnabled(false);
+	btnStop.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    sc.stopServer();
+		}
+	});
+	btnStop.setBounds(129, 126, 124, 23);
+	panel.add(btnStop);
 	setVisible(true);
     }
 }
