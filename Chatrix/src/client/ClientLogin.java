91bc8514d7dd3fc1fd404aa32c1e9e4ab7bd5551
Emil Thorenfeldt <emildinho@gmail.com>
2013-10-06 14:49:11 +0200

    ...

--- Chatrix/src/client/ClientLogin.java
+++ Chatrix/src/client/ClientLogin.java
@@ -10,80 +10,77 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class ClientLogin
-{
+public class ClientLogin {
 
-    private JFrame frame;
-    private JTextField textField;
-    private JPasswordField passwordField;
-    private ClientController controller;
-    private JLabel statusLbl;
-    private JButton btnRegister;
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private ClientController controller;
+	private JLabel statusLbl;
+	private JButton btnRegister;
 
-    /**
-     * Create the application.
-     */
-    public ClientLogin(ClientController controller)
-    {
-	this.controller = controller;
-	initialize();
-	frame.setVisible(true);
-    }
+	/**
+	 * Create the application.
+	 */
+	public ClientLogin(ClientController controller) {
+		this.controller = controller;
+		initialize();
+		frame.setVisible(true);
+	}
 
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize()
-    {
-	frame = new JFrame();
-	frame.setTitle("Logon");
-	frame.setBounds(100, 100, 221, 215);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.getContentPane().setLayout(null);
-	
-	JLabel lblUser = new JLabel("User");
-	lblUser.setBounds(29, 49, 59, 14);
-	frame.getContentPane().add(lblUser);
-	
-	JLabel lblPassword = new JLabel("Password");
-	lblPassword.setBounds(29, 81, 72, 14);
-	frame.getContentPane().add(lblPassword);
-	
-	textField = new JTextField();
-	textField.setBounds(100, 46, 86, 20);
-	frame.getContentPane().add(textField);
-	textField.setColumns(10);
-	
-	JButton btnLogin = new JButton("Login");
-	btnLogin.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-		    statusLbl.setText("Logging in..");
-		    try{
-			controller.login(textField.getText(),new String (passwordField.getPassword()));
-			new ClientGUI(controller);
-			frame.dispose();
-		    }		
-		    catch(ConnectException ce){
-			statusLbl.setText("Connection error..");
-		    }
-		    catch(IllegalArgumentException ia){
-			statusLbl.setText("Invalid credentials - please try again..");
-		    }
-		}
-	});
-	btnLogin.setBounds(97, 109, 89, 23);
-	frame.getContentPane().add(btnLogin);
-	
-	passwordField = new JPasswordField();
-	passwordField.setBounds(100, 78, 86, 20);
-	frame.getContentPane().add(passwordField);
-	
-	statusLbl = new JLabel("Please enter credentials");
-	statusLbl.setBounds(29, 11, 231, 14);
-	frame.getContentPane().add(statusLbl);
-	
-	btnRegister = new JButton("Register");
-	btnRegister.setBounds(100, 143, 89, 23);
-	frame.getContentPane().add(btnRegister);
-    }
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Logon");
+		frame.setBounds(100, 100, 221, 215);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JLabel lblUser = new JLabel("User");
+		lblUser.setBounds(29, 49, 59, 14);
+		frame.getContentPane().add(lblUser);
+
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(29, 81, 72, 14);
+		frame.getContentPane().add(lblPassword);
+
+		textField = new JTextField();
+		textField.setBounds(100, 46, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				statusLbl.setText("Logging in..");
+				try {
+					controller.login(textField.getText(), new String(
+							passwordField.getPassword()));
+					new ClientGUI(controller);
+					frame.dispose();
+				} catch (ConnectException ce) {
+					statusLbl.setText("Connection error..");
+				} catch (IllegalArgumentException ia) {
+					statusLbl
+							.setText("Invalid credentials - please try again..");
+				}
+			}
+		});
+		btnLogin.setBounds(97, 109, 89, 23);
+		frame.getContentPane().add(btnLogin);
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(100, 78, 86, 20);
+		frame.getContentPane().add(passwordField);
+
+		statusLbl = new JLabel("Please enter credentials");
+		statusLbl.setBounds(29, 11, 231, 14);
+		frame.getContentPane().add(statusLbl);
+
+		btnRegister = new JButton("Register");
+		btnRegister.setBounds(100, 143, 89, 23);
+		frame.getContentPane().add(btnRegister);
+	}
 }
