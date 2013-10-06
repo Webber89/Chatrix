67dd49114f3e5998f1733d816213a3b07257871b
Emil Thorenfeldt <emildinho@gmail.com>
2013-10-06 14:16:14 +0200

    sendAndLock() undo

--- Chatrix/src/communication/OutputConnection.java
+++ Chatrix/src/communication/OutputConnection.java
@@ -3,8 +3,6 @@ package communication;
 import java.io.IOException;
 
 public interface OutputConnection {
-	public void sendAndLock(String content);
-	
 	public void send(String content) throws IOException;
 
 	public void ping();
