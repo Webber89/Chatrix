a6283e77c6ceb9c3656d4b8b2c62fdb8aadefe3a
Emil Thorenfeldt <emildinho@gmail.com>
2013-10-07 11:20:11 +0200

    Added static block to message for initialisation from JSON

--- Chatrix/src/core/Message.java
+++ Chatrix/src/core/Message.java
@@ -1,7 +1,9 @@
 package core;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -10,7 +12,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Message {
 	private LinkedHashMap<String, String> keyValuePairs = new LinkedHashMap<String, String>();
 	private Type type;
-
+	// Map of all Types
+	private static final Map<String, Type> typeMap;
+	// Statically initialises map
+	static {
+		typeMap = new HashMap<String, Type>();
+		for (Type t : Type.values())
+			typeMap.put(t.type, t);
+	}
+	
 	public enum Type {
 		MESSAGE("MSG", 2), JOIN("JOIN", 2), ENTER("ENT", 1), QUIT("QUIT", 0), CREATE(
 				"CRT", 2), INFO("INFO", 0), INVITE("INV", 2);
@@ -30,6 +40,11 @@ public class Message {
 
 	public Message(Type type) {
 		this.type = type;
+		keyValuePairs.put("type", this.type.type);
+	}
+	
+	public Message(String type) {
+		this.type = getMessageTypeByName(type);
 		keyValuePairs.put("type", this.type.type);
 	}
 
@@ -56,5 +71,20 @@ public class Message {
 			throws JsonParseException, JsonMappingException, IOException {
 		return new ObjectMapper().readValue(json, LinkedHashMap.class);
 
+	}
+	
+	public static Message.Type getMessageTypeByName(String name) {
+		return typeMap.get(name);
+	}
+	
+	public static Message parseJSONtoMessage(String json) throws JsonParseException, JsonMappingException, IOException {
+		// Converts JSON string to HashMap
+		@SuppressWarnings("unchecked")
+		LinkedHashMap<String, String> map = new ObjectMapper().readValue(json, LinkedHashMap.class);
+		// Removes "type" key-value from map and uses returned value to initialise message
+		Message message = new Message(map.remove("type"));
+		// Sets message map as the remaining map contents
+		message.keyValuePairs = map;
+		return message;
 	}
 }
