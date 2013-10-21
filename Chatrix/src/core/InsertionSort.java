0628c6086040d5f6508033b841e8f24aea36394e
Emil Thorenfeldt <emildinho@gmail.com>
2013-10-21 16:58:25 +0200

    Initial implementation of room, join updated, sort updated

--- Chatrix/src/core/InsertionSort.java
+++ Chatrix/src/core/InsertionSort.java
@@ -1,23 +1,75 @@
 package core;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class InsertionSort {
 
-	public static void main(String[] args) {
-
-		ArrayList<Integer> a = new ArrayList<Integer>();
-		a.add(4);
-		a.add(452);
-		a.add(3);
-		a.add(664);
-		a.add(5);
-		a.add(1);
-		sortUsers(a);
+	public static void main(String[] lort) throws JsonParseException, JsonMappingException, IOException {
+//		test();
+		String[] strings = { "hans", "preben" };
+		System.out.println(Message.writeValueAsString(strings));
+		List<String> list = Message.toArrayList("[\"hans\"]");
+		System.out.println(list.get(0));
 	}
 
-	public InsertionSort(Collection<?> unsortedList) {
+	public static void test() {
+		ArrayList<String> liste = new ArrayList<String>();
+		liste.add("Poul");
+		liste.add("Brian");
+		liste.add("Hans");
+		liste.add("Emil");
+		liste = sortWithComp(liste, new Comparator<String>() {
+			
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		for (String s : liste) {
+			System.out.println(s);
+		}
+	}
+	
+	public static <T> ArrayList<T> sortWithComp(ArrayList<T> unsortedList, Comparator<T> comparator) {
+		ArrayList<T> sortedList = new ArrayList<T>(unsortedList);
+		for (int sortedElements = 1; sortedElements < sortedList.size(); sortedElements++) {
 
+			T itemToInsert = sortedList.get(sortedElements);
+			int pointer;
+
+			for (pointer = sortedElements - 1; (pointer >= 0)
+					&& (comparator.compare(sortedList.get(pointer), itemToInsert) > 0); pointer--) {
+				sortedList.set(pointer + 1, sortedList.get(pointer));
+			}
+
+			sortedList.set(pointer + 1, itemToInsert);
+		}
+		return sortedList;
+	}
+	
+	public static <T extends Comparable<T>> ArrayList<T> sortMoreUsers(
+			ArrayList<T> unsortedList) {
+		ArrayList<T> sortedList = new ArrayList<T>(unsortedList);
+
+		for (int sortedElements = 1; sortedElements < sortedList.size(); sortedElements++) {
+
+			T itemToInsert = sortedList.get(sortedElements);
+			int pointer;
+
+			for (pointer = sortedElements - 1; (pointer >= 0)
+					&& (sortedList.get(pointer).compareTo(itemToInsert) > 0); pointer--) {
+				sortedList.set(pointer + 1, sortedList.get(pointer));
+			}
+
+			sortedList.set(pointer + 1, itemToInsert);
+		}
+		return sortedList;
 	}
 
 	public static ArrayList<Integer> sortUsers(ArrayList<Integer> unsortedList) {
--- Chatrix/src/server/ServerController.java
+++ Chatrix/src/server/ServerController.java
@@ -14,199 +14,174 @@ import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.StringTokenizer;
+import java.util.UUID;
 
 import core.Message;
 
-public class ServerController
-{
-    private int port;
-    public String ip;
-    private ServerConnection serverCon;
-    private Thread serverThread;
-    private static HashMap<String, String> users = new HashMap<String, String>();
-    private static File userListFile = new File(System.getProperty("user.dir")
-	    + "/userlist.dat");
-
-    public ServerController()
-    {
-	loadUserList();
-    }
-
-    public void createServer(String conType, boolean isPrivate, int port)
-    {
-	this.port = port;
-	try
-	{
-	    setIP(isPrivate);
-	    broadcastIP(conType);
-	} catch (Exception e)
-	{
-	    e.printStackTrace();
+public class ServerController {
+	private int port;
+	public String ip;
+	private ServerConnection serverCon;
+	private Thread serverThread;
+	private static HashMap<String, String> users = new HashMap<String, String>();
+	private static File userListFile = new File(System.getProperty("user.dir")
+			+ "/userlist.dat");
+	private static HashMap<String, Room> rooms = new HashMap<String, Room>();
+	
+	public ServerController() {
+		loadUserList();
+		rooms.put("public", new Room("public"));
 	}
-	switch (conType)
-	{
-	case "TCP":
-	    serverCon = new ServerConnection(port);
-	    serverThread = new Thread(serverCon);
-	    serverThread.start();
-	    break;
-	case "UDP":
-	    break;
+
+	public void createServer(String conType, boolean isPrivate, int port) {
+		this.port = port;
+		try {
+			setIP(isPrivate);
+			broadcastIP(conType);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		switch (conType) {
+		case "TCP":
+			serverCon = new ServerConnection(port);
+			serverThread = new Thread(serverCon);
+			serverThread.start();
+			break;
+		case "UDP":
+			break;
+		}
 	}
-    }
 
-    protected void broadcastIP(String conType) throws Exception
-    {
-	try
-	{
-	    // Contacting server with cancel param - server removes previous
-	    // ip/port.
-	    URL cancelLink = new URL(
-		    "http://thebecw.appspot.com/updatelink?sae=chat&cancel=true");
-	    BufferedReader in1 = new BufferedReader(new InputStreamReader(
-		    cancelLink.openStream()));
-	    String cancelMsg = in1.readLine();
-	    // Checking if msg is OK or prompt with errormsg
-	    if (cancelMsg.equals("ok"))
-	    {
-	    } else
-	    {
-		throw new Exception("Unable to cancel broadcast link");
-	    }
-	    // Contacting server with params - setting the ip/port posted on
-	    // server
-	    // to the client using the updateLink
-	    URL updateLink = new URL(
-		    "http://thebecw.appspot.com/updatelink?sae=chat&link=" + ip
-			    + ":" + port + ":" + conType);
-	    BufferedReader in2 = new BufferedReader(new InputStreamReader(
-	    // Creating stream to server
-		    updateLink.openStream()));
-	    String linkMsg = in2.readLine();
-	    // Checking if OK - else prompt errorMSG
-	    if (linkMsg.equals("ok"))
-	    {
-	    } else
-	    {
-		throw new Exception("Unable to broadcast link");
-	    }
-	} catch (UnknownHostException e)
-	{
-	    Thread.sleep(500);
-	    System.out.println("Failed to access appspot, trying again ...");
-	    broadcastIP(conType);
+	protected void broadcastIP(String conType) throws Exception {
+		try {
+			// Contacting server with cancel param - server removes previous
+			// ip/port.
+			URL cancelLink = new URL(
+					"http://thebecw.appspot.com/updatelink?sae=chat&cancel=true");
+			BufferedReader in1 = new BufferedReader(new InputStreamReader(
+					cancelLink.openStream()));
+			String cancelMsg = in1.readLine();
+			// Checking if msg is OK or prompt with errormsg
+			if (cancelMsg.equals("ok")) {
+			} else {
+				throw new Exception("Unable to cancel broadcast link");
+			}
+			// Contacting server with params - setting the ip/port posted on
+			// server
+			// to the client using the updateLink
+			URL updateLink = new URL(
+					"http://thebecw.appspot.com/updatelink?sae=chat&link=" + ip
+							+ ":" + port + ":" + conType);
+			BufferedReader in2 = new BufferedReader(new InputStreamReader(
+			// Creating stream to server
+					updateLink.openStream()));
+			String linkMsg = in2.readLine();
+			// Checking if OK - else prompt errorMSG
+			if (linkMsg.equals("ok")) {
+			} else {
+				throw new Exception("Unable to broadcast link");
+			}
+		} catch (UnknownHostException e) {
+			Thread.sleep(500);
+			System.out.println("Failed to access appspot, trying again ...");
+			broadcastIP(conType);
+		}
+		getIP();
 	}
-	getIP();
-    }
 
-    private void setIP(boolean isPrivate) throws Exception
-    {
-	if (isPrivate)
-	{
-	    ip = InetAddress.getLocalHost().getHostAddress();
-	} else
-	{
-	    URL amazonIP = new URL("http://checkip.amazonaws.com");
-	    BufferedReader in = new BufferedReader(new InputStreamReader(
-		    amazonIP.openStream()));
-	    ip = in.readLine();
+	private void setIP(boolean isPrivate) throws Exception {
+		if (isPrivate) {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} else {
+			URL amazonIP = new URL("http://checkip.amazonaws.com");
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					amazonIP.openStream()));
+			ip = in.readLine();
+		}
 	}
-    }
 
-    protected void getIP() throws Exception
-    {
-	// Contacting server to retrieve stored ip/port
-	URL getLink = new URL(
-		"http://thebecw.appspot.com/spreadsheet?chat=true");
-	BufferedReader in3 = new BufferedReader(new InputStreamReader(
-		getLink.openStream()));
-	String serverAddress = in3.readLine();
-	StringTokenizer tokenizer = new StringTokenizer(serverAddress, ":");
-	// reading first token before ":" seperator = the ip
-	ip = tokenizer.nextToken();
-	// reading the port, which is the next token
-	port = Integer.parseInt(tokenizer.nextToken());
-	System.out.println(ip + ", " + port);
-    }
-
-    public void stopServer()
-    {
-	try
-	{
-	    System.out.println("Stopping server");
-	    serverCon.shutdown();
-	    // serverSocket.close();
-	} catch (Exception e)
-	{
-	    System.out.println("Failed to stop server");
+	protected void getIP() throws Exception {
+		// Contacting server to retrieve stored ip/port
+		URL getLink = new URL(
+				"http://thebecw.appspot.com/spreadsheet?chat=true");
+		BufferedReader in3 = new BufferedReader(new InputStreamReader(
+				getLink.openStream()));
+		String serverAddress = in3.readLine();
+		StringTokenizer tokenizer = new StringTokenizer(serverAddress, ":");
+		// reading first token before ":" seperator = the ip
+		ip = tokenizer.nextToken();
+		// reading the port, which is the next token
+		port = Integer.parseInt(tokenizer.nextToken());
+		System.out.println(ip + ", " + port);
 	}
-    }
 
-    public static Message sendMessage(Message message)
-    {
+	public void stopServer() {
+		try {
+			System.out.println("Stopping server");
+			serverCon.shutdown();
+			// serverSocket.close();
+		} catch (Exception e) {
+			System.out.println("Failed to stop server");
+		}
+	}
 
-	return message;
+	public static Message sendMessage(Message message) {
 
-    }
+		return message;
 
-    public static Message login(Message message)
-    {
-	String user = message.keyValuePairs.get("user");
-	String pass = message.keyValuePairs.get("pass");
-	Message returnMessage = new Message(Message.Type.SERVER_JOIN);
-	if (users.containsKey(user))
-	{
-	    if (users.get(user).equals(pass))
-	    {
-		returnMessage.addKeyValue("success", "true");
-		// TODO generate token
+	}
+
+	public static Message login(Message message, Client c) {
+		String user = message.keyValuePairs.get("user");
+		String pass = message.keyValuePairs.get("pass");
+		Message returnMessage = new Message(Message.Type.SERVER_JOIN);
+		if (users.containsKey(user)) {
+			if (users.get(user).equals(pass)) {
+				returnMessage.addKeyValue("success", "true");
+				returnMessage.addKeyValue("token", generateToken());
+				rooms.get("public").addClient(c);
+				return returnMessage;
+			}
+		}
+		returnMessage.addKeyValue("success", "false");
+		returnMessage.addKeyValue("message", "Invalid username or password");
 		return returnMessage;
-	    }
-	}
-	returnMessage.addKeyValue("success", "false");
-	returnMessage.addKeyValue("message", "Invalid username or password");
-	return returnMessage;
-    }
-
-    @SuppressWarnings("unchecked")
-    private static void loadUserList()
-    {
-	ObjectInputStream in;
-	try
-	{
-	    in = new ObjectInputStream(new FileInputStream(userListFile));
-	    users = (HashMap<String, String>) in.readObject();
-	    for (String s : users.keySet())
-		System.out.println("User: " + s);
-	} catch (FileNotFoundException e)
-	{
-	    System.out.println("Userlist does not exist");
-	} catch (IOException e)
-	{
-	    e.printStackTrace();
-	} catch (ClassNotFoundException e)
-	{
-	    e.printStackTrace();
 	}
 
-    }
-
-    @SuppressWarnings("unused")
-	private static void saveUserList()
-    {
-	ObjectOutputStream out;
-	try
-	{
-	    userListFile.createNewFile();
-	    out = new ObjectOutputStream(new FileOutputStream(userListFile));
-	    out.writeObject(users);
-	    out.close();
-	} catch (FileNotFoundException e)
-	{
-	    e.printStackTrace();
-	} catch (IOException e)
-	{
-	    e.printStackTrace();
+	private static String generateToken() {
+		return UUID.randomUUID().toString();
 	}
-    }
+
+	@SuppressWarnings("unchecked")
+	private static void loadUserList() {
+		ObjectInputStream in;
+		try {
+			in = new ObjectInputStream(new FileInputStream(userListFile));
+			users = (HashMap<String, String>) in.readObject();
+			for (String s : users.keySet())
+				System.out.println("User: " + s);
+		} catch (FileNotFoundException e) {
+			System.out.println("Userlist does not exist");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@SuppressWarnings("unused")
+	private static void saveUserList() {
+		ObjectOutputStream out;
+		try {
+			userListFile.createNewFile();
+			out = new ObjectOutputStream(new FileOutputStream(userListFile));
+			out.writeObject(users);
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
