fd1cbfcb08b8289cc377132b01fa9028bc56c8b6
Emil Thorenfeldt <emildinho@gmail.com>
2013-10-21 17:00:26 +0200

    unchanged change.

--- Chatrix/src/client/Room.java
+++ Chatrix/src/client/Room.java
@@ -2,26 +2,24 @@ package client;
 
 import java.util.ArrayList;
 
-public class Room
-{
-    private String roomName;
-    private ArrayList<String> userList = new ArrayList<String>();
+@SuppressWarnings("unused")
+public class Room {
+	private String roomName;
+	private ArrayList<String> userList = new ArrayList<String>();
 
-    public Room()
-    {
-    }
+	public Room() {
+	}
 
-    public Room(String roomName)
-    {
-	this.roomName = roomName;
-	
-    }
+	public Room(String roomName) {
+		this.roomName = roomName;
 
-    public void updateUserList(ArrayList<String> users){
-	
-	this.userList = users;
-//	userList.put(msg.getValue("users"));
-//	TODO update userlist of the room.
-	
-    }
+	}
+
+	public void updateUserList(ArrayList<String> users) {
+
+		this.userList = users;
+		// userList.put(msg.getValue("users"));
+		// TODO update userlist of the room.
+
+	}
 }
